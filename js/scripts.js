fa066ffdad793fc63e058954dc067860a4662427
Mikkel Hauvgaard <mikkelhauvgaard@gmail.com>
2018-09-20 13:15:55 +0200

    Doesn't work as expected. will fix later

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@
 var host = "codercob.dk";
 if ((host == window.location.host) && (window.location.protocol != "https:"))
     window.location.protocol = "https";
+
+//$(document).ready(function() {
+//    $('#MENU').load('Menubar.html');
+//});
+
+
