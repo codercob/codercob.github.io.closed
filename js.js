1225eb42622cb9c9136ae221fdb9ca79560bd7d1
Yosefmeiz <43385764+Yosefmeiz@users.noreply.github.com>
2019-05-14 11:51:48 +0200

    Update js.js

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ function getFormula(which) {
       document.getElementById('formula').innerHTML="<img src='billeder/1.png' width='200'></img> <br> eller <br> <img src='billeder/2.png' width='200'></img> <br> eller <br> <img src='billeder/3.png' width='200'></img>";
       break;
     case "rightcathode":
-      document.getElementById('formula').innerHTML="<img src='billeder/4.png' width='200'></img> <br> eller <br> <img src='billeder/5.png' width='200'></img> <br> eller <br> <img src='billeder/6.png' width='200'></img> <br> eller <br> <img src='billeder/7.png' width='200'></img> <br> eller <br> <img src='billeder/8.png' width='200'></img>";
+      document.getElementById('formula').innerHTML="<img src='billeder/a4.png' width='200'></img> <br> eller <br> <img src='billeder/5.png' width='200'></img> <br> eller <br> <img src='billeder/6.png' width='200'></img> <br> eller <br> <img src='billeder/7.png' width='200'></img> <br> eller <br> <img src='billeder/8.png' width='200'></img>";
       break;
     case "lowercathode":
       document.getElementById('formula').innerHTML="<img src='billeder/9.png' width='200'></img> <br> eller <br> <img src='billeder/10.png' width='200'></img> <br> eller <br> <img src='billeder/11.png' width='200'></img> <br> eller <br> <img src='billeder/12.png' width='200'></img> <br> eller <br> <img src='billeder/13.png' width='200'></img>";
